97dda7e0f46f28b2ccafea382d0ffe577736149a
Maria Gabriela Reis <mariagabrielagreis@gmail.com>
2022-06-23 12:03:21 -0300

    :recycle: refactor: remove unused variables

--- src/connectDatabase.py
+++ src/connectDatabase.py
@@ -1,6 +1,5 @@
 from pymongo import MongoClient
 import redis
-from astrapy.client import create_astra_client
 
 def connectToMongoDB():
   db = MongoClient("mongodb+srv://<user>:<password>@fa-starting-no-sql.6vnsq.mongodb.net/")
@@ -14,18 +13,3 @@ def connectToRedis():
   )
 
   return db
-
-# get Astra connection information from environment variables
-ASTRA_DB_ID = ''
-ASTRA_DB_REGION = ''
-ASTRA_DB_APPLICATION_TOKEN = ''
-
-def connectToCassandra():
-  # setup an Astra Client and create a shortcut to our test colllection
-  astra_client = create_astra_client(
-    astra_database_id=ASTRA_DB_ID,
-    astra_database_region=ASTRA_DB_REGION,
-    astra_application_token=ASTRA_DB_APPLICATION_TOKEN
-  )
-
-  return astra_client.rest
