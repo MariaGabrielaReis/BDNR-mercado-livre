e777b6eb79dee70b4d2034e277e9a879a82463c4
Maria Gabriela Reis <mariagabrielagreis@gmail.com>
2022-06-23 11:01:40 -0300

    :fire: fix: update prop name for add products to bag

--- src/controllers/userController.py
+++ src/controllers/userController.py
@@ -14,35 +14,38 @@ def index():
 def show(params):
     cpf = params.get("cpf")
     user = userCollection.find_one({ "cpf": cpf })
-    
+
     return json.dumps(user, default=str)    
 
 def create(request):
     user = request.get_json()
     userCollection.insert_one(user)
+
     return json.dumps({"status": "OK"})
 
 def update(request):
     updatedUser = request.get_json()
     updatedUserData = {"$set": {"nome": updatedUser["nome"], "email": updatedUser["email"], "endereco": updatedUser["endereco"]}}
     userCollection.update_one({ "cpf": updatedUser["cpf"] }, updatedUserData)
+
     return json.dumps({"status": "OK"})
     
 def addToBag(params, request):
-     userCpf = params.get("cpf")
-     buy = request.get_json()
-     user = userCollection.find_one({ "cpf": userCpf })
-     
-     newBuy = {"$set": {"customer": buy["customer"], "product": buy["product"], "total": buy["total"], "paymentType": buy["paymentType"]}}
-     allBuys = user["buys"]
-     allBuys.append(newBuy)
+    userCpf = params.get("cpf")
+    product = request.get_json()
+    user = userCollection.find_one({ "cpf": userCpf })
 
-     user = {"$set": {"buys": allBuys}}
+    newProduct = {"$set": {"_id": product["id"], "name": product["name"], "description": product["description"], "price": product["price"]}}
+    allProducts = user["bag"]
+    allProducts.append(newProduct)
 
-     userCollection.update_one({ "cpf":userCpf }, user)
-     return json.dumps({"status": "OK"})
+    user = {"$set": {"bag": newProduct}}
+    userCollection.update_one({ "cpf": userCpf }, user)
+
+    return json.dumps({"status": "OK"})
 
 def delete(params):
     cpf = params.get("cpf")
     userCollection.delete_one({"cpf": cpf})
+    
     return json.dumps({"status": "OK"})
