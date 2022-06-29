059a13cab71df283f56552a650be9f8b14a39d82
Maria Gabriela Reis <mariagabrielagreis@gmail.com>
2022-06-29 15:28:35 -0300

    :sparkles: feat: add routes for create, view and delete wishlist from Cassandra

--- src/connectDatabase.py
+++ src/connectDatabase.py
@@ -1,15 +1,21 @@
 from pymongo import MongoClient
 import redis
+from cassandra.cluster import Cluster
+from cassandra.auth import PlainTextAuthProvider
 
 def connectToMongoDB():
-  db = MongoClient("mongodb+srv://<user>:<password>@fa-starting-no-sql.6vnsq.mongodb.net/")
+  db = MongoClient('mongodb+srv://<user>:<password>@fa-starting-no-sql.6vnsq.mongodb.net/')
+
   return db['mercado-livre']
 
 def connectToRedis():
-  db = redis.Redis(
-    host='<host>',
-    port="<port-number>",
-    password='<password>'
-  )
+  db = redis.Redis(host='<host>', port="<port-number>", password='<password>')
+
+  return db
+
+def connectToCassandra():
+  auth_provider = PlainTextAuthProvider('<user>', '<password>')
+  cluster = Cluster(auth_provider=auth_provider)
+  db = cluster.connect()
 
   return db
--- src/controllers/wishlistController.py
+++ src/controllers/wishlistController.py
@@ -5,13 +5,15 @@ mongoDB = connectDB.connectToMongoDB()
 userCollection = mongoDB.usuario
 
 redis = connectDB.connectToRedis()
+
+cassandra = connectDB.connectToCassandra()
   
 def addWishlistToRedis(params):
   userCpf = params.get("userCpf")
   user = userCollection.find_one({ "cpf": userCpf })
   redis.set(f'wishlist:{userCpf}', user['wishlist'])
 
-  return json.dumps({"status": "ok"})
+  return json.dumps({"status": "OK"})
 
 def showRedisWishlist(params):
   userCpf = params.get("userCpf")  
@@ -24,5 +26,32 @@ def showRedisWishlist(params):
 def deleteWishlistFromRedis(params):
   userCpf = params.get("userCpf")
   redis.delete(f'wishlist:{userCpf}')
+
+  return json.dumps({"status": "OK"}) 
+ 
+def addWishlistToCassandra(params):
+  userCpf = params.get("userCpf")
+  redisWishlist = redis.get(f'wishlist:{userCpf}')
+
+  cassandra.execute(f'INSERT INTO mercadolivre.wishlist (userCpf, products) VALUES ({userCpf}, {str(redisWishlist)})')
+
+  return json.dumps({"status": "OK"})
+
+def showCassandraWishlist(params):
+  userCpf = params.get("userCpf")
+  response = cassandra.execute(f'SELECT * FROM mercadolivre.wishlist WHERE userCpf = {userCpf}')
+  
+  if (not response):
+    return json.dumps({"hasError": True, "Message": "Lista de desejos não encontrada!"})
+
+  response = response[0]
+  return json.dumps({
+    "userCpf": response.userCpf,
+    "products": json.dumps({response.products})
+  })
+
+def deleteWishlistFromCassandra(params):
+  userCpf = params.get("userCpf")
+  cassandra.execute(f'DELETE FROM mercadolivre.wishlist WHERE userCpf = {userCpf} IF EXISTS')
 
   return json.dumps({"status": "OK"}) 
